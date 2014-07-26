06a01485a3094f2ce2c5ef16a37c9b6b2af01b42
m1ron <m1ron.com@gmail.com>
2014-07-26 13:43:16 +0300

    #2 #chance - added bxSlider script

--- chance/js/main.js
+++ chance/js/main.js
@@ -1,1 +1,13 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true */
+/*global window, console, document, $, jQuery, PIE */
 
+/* On document ready */
+$(document).ready(function () {
+	$('.image-carousel .carousel').bxSlider({
+		controls: false,
+		auto: true,
+		pause: 5000,
+		speed: 700,
+		useCSS: false
+	});
+});
