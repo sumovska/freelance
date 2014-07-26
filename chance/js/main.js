089b8b3d6ec56efb3f175980540cc8e113d908e7
Nataliia <sumovskaya@gmail.com>
2014-07-26 23:50:04 +0300

    #2 #chance - added slider script

--- chance/js/main.js
+++ chance/js/main.js
@@ -10,4 +10,27 @@ $(document).ready(function () {
 		speed: 700,
 		useCSS: false
 	});
-});
+	$('.block-partners .carousel').bxSlider({
+		infiniteLoop: false,
+		pager: false,
+		slideWidth: 115,
+		minSlides: 6,
+    	maxSlides: 6,
+    	slideMargin: 5
+	});
+	$('.slider-photo .screen .carousel').bxSlider({
+		auto: true,
+		pager: false,
+		pause: 5000,
+		speed: 700,
+		useCSS: false
+	});
+	$('.slider-photo .scroll .carousel').bxSlider({
+		infiniteLoop: false,
+		pager: false,
+		slideWidth: 115,
+		minSlides: 14,
+    	maxSlides: 14,
+    	slideMargin: 5
+	});
+})
