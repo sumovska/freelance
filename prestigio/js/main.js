da3571b7d3c267e85ad33be4293a745f8766419e
Nataliia <sumovskaya@gmail.com>
2014-08-15 01:23:37 +0300

    #4 #prestigio

---
+++ prestigio/js/main.js
@@ -0,0 +1,14 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true */
+/*global window, console, document, $, jQuery, PIE */
+
+/* On document ready */
+$(document).ready(function () {
+
+	var imageCarousel = $('.promo .carousel');
+	imageCarousel.bxSlider({
+		controls: false,
+		auto: true,
+		pause: 5000,
+		speed: 700,
+		useCSS: false
+	});
