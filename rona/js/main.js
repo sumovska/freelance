327892dbbcaf97d40db5de4aea8948a3892a0a38
Nataliia <sumovskaya@gmail.com>
2014-10-10 13:04:45 +0300

    rona

---
+++ rona/js/main.js
@@ -0,0 +1,13 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true */
+/*global window, console, document, $, jQuery, PIE */
+
+
+/* On document ready */
+$(document).ready(function () {
+	$('.submenu-main a.open-list').click(function () {
+		$(this).closest('li').toggleClass('active');
+		return false;
+	});
+});
+
+
