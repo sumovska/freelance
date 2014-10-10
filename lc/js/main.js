d4b3279cc806b14ab76dc7305710ff354c35baad
Sergiy Miroshnychenko <m1ron.com@gmail.com>
2014-10-10 13:07:12 +0300

    #lc - fixes

--- lc/js/main.js
+++ lc/js/main.js
@@ -1,29 +1,95 @@
 /*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true */
 /*global window, console, document, $, jQuery, PIE */
 
-
 /* On document ready */
 $(document).ready(function () {
+	var body = $('body');
+
+	/* Инициализация форм */
+	$(':input').styler();
+
+	/* Попап */
+	$(".fancybox").fancybox();
+
+	/* Меню брендов */
+	$('.brands').each(function () {
+		$('li', this).each(function () {
+			if ($(this).children('ul').length > 0) {
+				$(this).addClass('sub');
+			}
+		});
+		$('li.sub > a', this).click(function () {
+			$(this).closest('li').toggleClass('open').siblings('.open').removeClass('open');
+			return false;
+		});
+	});
+	body.bind('mousedown', function (e) {
+		if (($(e.target).closest('.sub').length < 1) && (!$(e.target).is('.brands'))) {
+			$('.brands li.open').removeClass('open');
+		}
+	});
+
+	/* Субменю */
+	$('.submenu').each(function () {
+		$('li', this).each(function () {
+			if ($(this).children('ul').length > 0) {
+				$(this).addClass('sub');
+			}
+		});
+		$('li.sub > a', this).click(function () {
+			$(this).siblings('ul').slideToggle(200, function () {
+				$(this).closest('li').toggleClass('open');
+			});
+			return false;
+		});
+	});
+
+	/* Карусель */
 	$('.carousel .catalog').bxSlider({
-		infiniteLoop: false,
+		infiniteLoop: true,
 		useCSS: false,
 		pager: false,
 		slideWidth: 219,
-		minSlides: 4,
-		maxSliders: 4,
+		maxSlides: 4,
 		moveSlides: 1
 	});
-	$('.submenu a.open-list').click(function () {
-		$(this).closest('li').toggleClass('active');
-		return false;
+
+	/* Переключение чата */
+	$('.chat').each(function () {
+		var _self = $(this);
+		$('.toggle', this).click(function () {
+			if (_self.is('.chat-open')) {
+				_self.animate({
+					width: '34px'
+				}, function () {
+					_self.removeClass('chat-open');
+				});
+			} else {
+				_self.animate({
+					width: '304px'
+				}, function () {
+					_self.addClass('chat-open');
+				});
+			}
+			return false;
+		});
 	});
-	$('.brand-navigation a.open-list').click(function () {
-		$(this).closest('li').toggleClass('active');
-		return false;
+	body.bind('mousedown', function (e) {
+		if (($(e.target).closest('.chat').length < 1)) {
+			$('.chat-open').animate({
+				width: '34px'
+			}, function () {
+				$('.chat-open').removeClass('chat-open');
+			});
+		}
 	});
-	$('.catalog .item .sale .new-price').prepend('<i class="before"></i>');
-	$('').append('<i class="after"></i>');
-	$('..crumbs-title .name:last-child').addClass('last-child');
-	$('.community-group .member-link:nth-child(3n)').addClass('nth-child-3n');
+
+	/* IE Fixes */
+	if ($.browser.msie) {
+		if ($.browser.versionNumber < 9) {
+			$('.crumbs-title .name:last-child').addClass('last-child');
+			$('.community-group .member-link:nth-child(3n)').addClass('nth-child-3n');
+		}
+	}
 });
 
