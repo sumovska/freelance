0c12cab187be41f338e8c1ecfd30236f98d66e88
Sergiy Miroshnychenko <m1ron.com@gmail.com>
2014-10-30 13:14:05 +0200

    #boson - initial version

--- boson/js/main.js
+++ boson/js/main.js
@@ -4,8 +4,18 @@
 /* On document ready */
 $(document).ready(function () {
 
-	var imageCarousel = $('.image-carousel .carousel');
-	imageCarousel.bxSlider({
+	/* Инициализация блока видео */
+	if ($('.index').length > 0) {
+		window.video = $('.index .video .resize').eq(0);
+		window.video.css({
+			'width': 'auto',
+			'top': '50%',
+			'left': '50%'
+		})
+	}
+	$(window).resize();
+
+	$('.image-carousel .carousel').bxSlider({
 		infiniteLoop: false,
 		pager: false,
 		slideWidth: 276,
@@ -14,6 +24,58 @@ $(document).ready(function () {
 		slideMargin: 52,
 		moveSlides: 1
 	});
-
-	
 });
+
+$(window).resize(function () {
+	/* Изменение размеров видео на главной */
+	if (typeof window.video != 'undefined') {
+		if (window.video.width() <= window.video.parent().width()) {
+			window.video.width('100%').height('auto');
+		}
+		if (window.video.height() <= window.video.parent().height()) {
+			window.video.width('auto').height('100%');
+		}
+		window.video.css({
+			'margin-left': '-' + Math.floor(+window.video.width() / 2) + 'px',
+			'margin-top': '-' + Math.floor(+window.video.height() / 2) + 'px'
+		})
+	}
+});
+
+window.lastScrollTop = 0;
+window.scrolling = true;
+$(window).on('scroll touchmove', function () {
+	return scrollEvent();
+});
+
+/* Обработчики скролла */
+function scrollEvent() {
+	var current = $(window).scrollTop(), body, h = 0;
+	/* Переключение плавающего хедера */
+	if (current > 15) {
+		$('.header').addClass('header-fixed');
+	} else {
+		$('.header').removeClass('header-fixed');
+	}
+	if (typeof window.video != 'undefined') {
+		/* скролл до нужного места */
+		h = $(window).height() - 40;
+		if ((current > 1) && (current < (h - 1)) && scrolling) {
+			$(window).disablescroll();
+			scrolling = false;
+			if (current <= lastScrollTop) {
+				h = 0;
+			}
+			$('html, body').animate({
+				scrollTop: h
+			}, 500, 'swing', function () {
+				$(window).disablescroll('undo');
+				scrolling = true;
+			});
+		}
+	}
+	lastScrollTop = current;
+	if (!scrolling) {
+		return false;
+	}
+}
