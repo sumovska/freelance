a83825583c9b9d307869b920f4d83c5b159c4bdb
Sergiy Miroshnychenko <m1ron.com@gmail.com>
2014-11-05 15:08:51 +0200

    #boson - initial

--- boson/js/main.js
+++ boson/js/main.js
@@ -11,30 +11,45 @@ $(document).ready(function () {
 			'width': 'auto',
 			'top': '50%',
 			'left': '50%'
-		})
+		});
+		$('.index .down').click(function () {
+			$(window).scrollTop(5);
+			return false;
+		});
 	}
 	$(window).resize();
 
-	$('.image-carousel .carousel').bxSlider({
-		infiniteLoop: false,
+	$('.clients .carousel').bxSlider({
+		infiniteLoop: true,
 		pager: false,
-		slideWidth: 276,
-		minSlides: 6,
-		maxSlides: 6,
-		slideMargin: 52,
+		slideWidth: 240,
+		minSlides: 1,
+		maxSlides: 5,
+		slideMargin: 0,
 		moveSlides: 1
 	});
 
-	$('.project-carousel .carousel').bxSlider({
-		auto: true,
-		infiniteLoop: false,
-		slideWidth: 1200,
-		minSlides: 1,
-		maxSlides: 1,
-		slideMargin: 52,
-		moveSlides: 1,
-		speed: 700
+	$('.project-carousel .carousel').bxSlider();
+
+	$('.header .nav').each(function () {
+		var _self = $(this);
+		$(this).append('<a class="toggle" href="#"></a>');
+		$('.toggle', this).click(function () {
+			$('ul', _self).fadeToggle(200);
+			$('body').toggleClass('body-nav-open');
+			return false;
+		});
 	});
+
+	$('body').bind("click touchstart", function (e) {
+		if ($(this).hasClass('body-nav-open')) {
+			if (($(e.target).is('.nav ul'))) {
+				$(e.target).fadeOut(200);
+				$('body').removeClass('body-nav-open');
+			}
+		}
+	});
+
 });
 
 $(window).resize(function () {
@@ -69,20 +84,22 @@ function scrollEvent() {
 		$('.header').removeClass('header-fixed');
 	}
 	if (typeof window.video != 'undefined') {
-		/* скролл до нужного места */
-		h = $(window).height() - 40;
-		if ((current > 1) && (current < (h - 1)) && scrolling) {
-			$(window).disablescroll();
-			scrolling = false;
-			if (current <= lastScrollTop) {
-				h = 0;
+		if ($(window).width() > 1024) {
+			/* скролл до нужного места */
+			h = $(window).height() - 40;
+			if ((current > 1) && (current < (h - 1)) && scrolling) {
+				$(window).disablescroll();
+				scrolling = false;
+				if (current <= lastScrollTop) {
+					h = 0;
+				}
+				$('html, body').animate({
+					scrollTop: h
+				}, 500, 'swing', function () {
+					$(window).disablescroll('undo');
+					scrolling = true;
+				});
 			}
-			$('html, body').animate({
-				scrollTop: h
-			}, 500, 'swing', function () {
-				$(window).disablescroll('undo');
-				scrolling = true;
-			});
 		}
 	}
 	lastScrollTop = current;
