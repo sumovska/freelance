0d1394bccb2ee4051067896232e45d3e1945258f
Sergiy Miroshnychenko <m1ron.com@gmail.com>
2014-11-08 14:54:06 +0200

    #duckling - #fixes

--- duckling/js/main.js
+++ duckling/js/main.js
@@ -4,4 +4,41 @@
 /* On document ready */
 $(document).ready(function () {
 
+	/* Прелоад */
+	$('.index').each(function () {
+		var _self = $(this);
+		$("<img />").attr("src", "img/index.jpg").one("load",function () {
+			_self.addClass('index-loaded');
+		}).each(function () {
+				if (this.complete) {
+					$(this).load()
+				}
+			});
+	});
+
+	/* Прокрутка */
+	$('.anchor').click(function () {
+		var where = '#' + $(this).attr('href').replace(/^.*#(.*)/, "$1");
+		d = Math.floor(Math.abs((+$(window).scrollTop() - +$(where).offset().top) * .8));
+		if (d < 500) {
+			d = 500;
+		}
+		if (d > 2000) {
+			d = 2000;
+		}
+		/*history.pushState(null, null, where);*/
+		$.scrollTo(where, {
+			duration: d,
+			easing: 'swing'
+		});
+		return false;
+	});
+
+	/* Лайтбокс */
+	$(".gallery .list .link").fancybox({
+		padding: 0,
+		prevEffect: 'fade',
+		nextEffect: 'fade',
+		closeBtn: false
+	});
 });
