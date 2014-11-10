2a87218be1da487742197fdfbbb7f51c1e3d4d3d
Sergiy Miroshnychenko <m1ron.com@gmail.com>
2014-11-11 00:57:25 +0200

    #landingi - #fixes

--- landingi/js/main.js
+++ landingi/js/main.js
@@ -3,5 +3,19 @@
 
 /* On document ready */
 $(document).ready(function () {
-
+	$('body').append('<div class="overlay"></div>');
+	$('.overlay').click(function () {
+		var _self = $(this);
+		$('.popup').fadeOut(300, 'swing', function () {
+			_self.fadeOut(300, 'swing');
+		});
+	});
+	$('.js-popup').click(function () {
+		var where = $('.' + $(this).attr('href').replace(/^.*#(.*)/, "$1"));
+		$('.overlay').fadeIn(300, 'swing', function () {
+			where.css('margin-left', -Math.floor(where.outerWidth() / 2)).css('margin-top', -Math.floor(where.outerHeight() / 2));
+			where.fadeIn(300, 'swing');
+		});
+		return false;
+	});
 });
