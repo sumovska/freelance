71c1bfa75f3dac109e60cf554ff920bfce4c3192
Nataliia <sumovskaya@gmail.com>
2014-11-19 19:47:24 +0200

    #boson fixes

--- boson/js/main.js
+++ boson/js/main.js
@@ -56,6 +56,23 @@ $(document).ready(function () {
 		}
 	});
 
+	/* Popup */
+	$('body').append('<div class="overlay"></div>');
+	$('.overlay').click(function () {
+		var _self = $(this);
+		$('.popup').fadeOut(300, 'swing', function () {
+			_self.fadeOut(300, 'swing');
+		});
+	});
+	$('.js-popup').click(function () {
+		var where = $('.' + $(this).attr('href').replace(/^.*#(.*)/, "$1"));
+		$('.overlay').fadeIn(300, 'swing', function () {
+			where.css('margin-left', -Math.floor(where.outerWidth() / 2)).css('margin-top', -Math.floor(where.outerHeight() / 2));
+			where.fadeIn(300, 'swing');
+		});
+		return false;
+	});
+
 });
 
 $(window).resize(function () {
