59ac555f417590d3c2386785281c716eb6e014ad
Nataliia <sumovskaya@gmail.com>
2014-12-05 13:37:46 +0200

    #astra - initial

--- astra/js/main.js
+++ astra/js/main.js
@@ -9,9 +9,31 @@ $(document).ready(function () {
 		useCSS: false,
 		auto: true,
 		speed: 700,
-		pager: false,
 		slideWidth: 1260,
 		minSlides: 1,
 		moveSlides: 1
+	});
+	$('body').append('<div class="overlay"></div>');
+	$('.overlay').click(function () {
+		var _self = $(this);
+		$('.popup').fadeOut(300, 'swing', function () {
+			_self.fadeOut(300, 'swing');
+		});
+	});
+	$('.js-popup').click(function () {
+		var where = $('.' + $(this).attr('href').replace(/^.*#(.*)/, "$1"));
+		$('.overlay').fadeIn(300, 'swing', function () {
+			where.css('margin-left', -Math.floor(where.outerWidth() / 2));
+			where.fadeIn(300, 'swing');
+		});
+		return false;
+	});
+	$('.news .unit .text').each(function () {
+		var _self = $(this);
+		$('.link', this).click(function () {
+			_self.toggleClass('text-open');
+			$('.hidden', _self).slideToggle(400);
+			return false;
+		});
 	});
 });
