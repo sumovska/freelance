ca47e62a02376a0c8d5598302f155d9223530860
Nataliia <sumovskaya@gmail.com>
2014-12-14 13:39:11 +0200

    #ehrle

---
+++ ehrle/js/main.js
@@ -0,0 +1,7 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true */
+/*global window, console, document, $, jQuery, PIE */
+
+/* On document ready */
+$(document).ready(function () {
+	
+}
