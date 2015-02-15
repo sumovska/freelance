0706d3d44e4262cd48ec5cda43b9327fdb983703
Nataliia <sumovskaya@gmail.com>
2015-02-15 16:32:23 +0200

    #four-sevenths

---
+++ Four Sevenths/js/main.js
@@ -0,0 +1,53 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true */
+/*global window, console, document, $, jQuery, PIE */
+
+/* On document ready */
+$(document).ready(function () {
+
+	/* Init forms */
+	$('input, select').styler();
+	
+	/* Gallery */
+	$('.gallery').each(function () {
+		$('.fancybox-gallery', this).fancybox({
+			autoSize: false,
+			fitToView: false,
+			padding: 0,
+			helpers: {
+				media: {}
+			}
+		});
+	});
+
+	/* Fancybox script */
+	$('.fancybox, .fancybox-popup').fancybox({
+		margin: 40,
+		nextEffect: 'fade',
+		prevEffect: 'fade',
+		helpers: {
+			overlay: {
+				speedIn: 250,
+				css: {
+					'background': 'rgba(0, 0, 0, 0.9)'
+				}
+			}
+		}
+	});
+
+	$('.block-item .triggers').each(function () {
+		$('li a', this).click(function () {
+			var where = $(this).attr("href").replace(/^.*#(.*)/, "$1");
+			$(this).closest('li').addClass('active').siblings('li.active').removeClass('active');
+			$('.tab-' + where).removeClass('tab-hidden').siblings('.tab').addClass('tab-hidden');
+			return false;
+		});
+	});
+
+	$('.block-item .tab').each(function () {
+		$('.link', this).click(function () {
+			$(this).siblings('.info').fadeToggle(400).closest('.line').toggleClass('line-active');
+			return false;
+		});
+	});
+
+});
