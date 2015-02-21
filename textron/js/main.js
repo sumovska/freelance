b3ed0e66649280950c5293e325ef5c32473c554f
Sergiy Miroshnychenko <m1ron.com@gmail.com>
2015-02-21 14:55:20 +0200

    #textron - fixes

---
+++ textron/js/main.js
@@ -0,0 +1,9 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true */
+/*global window, console, document, $, jQuery, PIE */
+
+/* On document ready */
+$(document).ready(function () {
+
+	/* Init forms */
+	$('input, select').styler();
+});
