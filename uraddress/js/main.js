ee10a2078642a55e8dd9e9653266b69793945586
Nataliia <sumovskaya@gmail.com>
2015-04-08 14:05:11 +0300

    #mobile - fixes

--- uraddress/js/main.js
+++ uraddress/js/main.js
@@ -31,4 +31,27 @@ $(document).ready(function () {
 		});
 	});
 
+	$('.nav').each(function () {
+		var _self = $(this);
+		$(this).append('<span class="toggle"></span>');
+		$('.toggle', this).click(function () {
+			if ($('.nav-space', _self).length === 0) {
+				$('.list', _self).wrap('<div class="nav-space"></div>')
+			}
+			$('.nav-space', _self).fadeToggle(200);
+			$('body').toggleClass('body-nav-open');
+			return false;
+		});
+	});
+
+	$('body').click(function() {
+		if ($(this).hasClass('body-nav-open')) {
+			if (($(e.target).is('.nav .list'))) {
+				$(e.target).closest('.nav-space').fadeOut(200);
+				$('body').removeClass('body-nav-open');
+			}
+		}
+	});
+
+
 });
