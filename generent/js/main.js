4f3b399064c88f20897e6ba654a4c5d2d77cd83f
Nataliia <sumovskaya@gmail.com>
2015-04-15 21:09:13 +0300

    #generent - text-page

---
+++ generent/js/main.js
@@ -0,0 +1,30 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true, node: true */
+/*global window, console, document, $, jQuery, PIE, initialize, styler */
+
+function initForms() {
+	$('input, select').styler();
+}
+
+/* On document ready */
+$(document).ready(function () {
+
+	$('.tabs').each(function () {
+		$('li a', this).click(function () {
+			var where = $(this).attr("href").replace(/^.*#(.*)/, "$1");
+			$(this).closest('li').addClass('active').siblings('li').removeClass('active');
+			$('#' + where).removeClass('tab-hidden').siblings('.tab').addClass('tab-hidden');
+			return false;
+		});
+	});
+
+	$('.slider-small').each(function () {
+		$('.slider-in').slick({
+			arrows: false,
+			dots: true
+		});
+		$('.slick-dots button').remove();
+		$('.slick-dots li').append('<span class="dots"></span>');
+	});
+
+});
+
